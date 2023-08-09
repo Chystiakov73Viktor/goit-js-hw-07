2687336925fb23c551d18939bddd98ba16524dc1
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-08-10 01:18:17 +0300

    добавлено код js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listElement = document.querySelector(".gallery");
+const itemElement = createGalleryItems(galleryItems);
+listElement.insertAdjacentHTML("beforeend", itemElement);
+listElement.addEventListener("click", onImgClick);
+
+function createGalleryItems(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+function onImgClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const inStance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="1280" height="auto">`);
+  inStance.show();
+  listElement.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      inStance.close();
+    }
+  });
+}
 
 console.log(galleryItems);
